cc4453103fa89c6d9ea5f6ffe2a448ef7dbf9bb8
JManuelMtz <j-manuel_mtz23@hotmail.com>
2016-03-31 16:58:05 -0600

    Modificada la presentacion de los elementos, que no perntecen a ningun tipo.

--- app/controllers.js
+++ app/controllers.js
@@ -3,57 +3,59 @@
 
     angular.module('App')
 
-    .controller('AppCtrl', ['$scope','TP','LxDialogService','LxNotificationService',function ($scope,TP,LxDialogService,LxNotificationService) {
-        $scope.vm = this;
-        $scope.vm.clasificacion = 'todos';
-        $scope.vm.dialogId = 'detalles-elemento';
+    .controller('AppCtrl', ['$scope','TP','LxDialogService','LxNotificationService',
+        function ($scope,TP,LxDialogService,LxNotificationService) {
+            $scope.vm = this;
+            $scope.vm.clasificacion = 'todos';
+            $scope.vm.dialogId = 'detalles-elemento';
 
-        // Almacena temporalmente los datos.
-        $scope.models = {};
-        $scope.elementos = [];
+            // Almacena temporalmente los datos.
+            $scope.models = {};
+            $scope.elementos = [];
 
-        // Carga los datos de un Service
-        $scope.loadData = function (indice) {
-            if($scope.models[indice]) {
-                $scope.vm.tabs = $scope.models[indice];
-                $scope.vm.loading = false;
-            } else {
-                TP.service(indice).query(function (response) {
-                    $scope.vm.tabs = response;
-                    $scope.models[indice] = response;
-                },function (response) {
-                    $scope.vm.error = response;
-                }).$promise.finally(function () {
+            // Carga los datos de un Service
+            $scope.loadData = function (indice) {
+                if($scope.models[indice]) {
+                    $scope.vm.tabs = $scope.models[indice];
                     $scope.vm.loading = false;
-                });
+                } else {
+                    TP.service(indice).query(function (response) {
+                        $scope.vm.tabs = response;
+                        $scope.models[indice] = response;
+                    },function (response) {
+                        $scope.vm.error = response;
+                    }).$promise.finally(function () {
+                        $scope.vm.loading = false;
+                    });
+                }
             }
-        }
 
-        $scope.getElemento = function (id) {
-            if($scope.elementos[id]) {
-                $scope.elemento = $scope.elementos[id];
-                $scope.showElemento($scope.elemento);
-            } else { 
-                LxNotificationService.info('Cargando elemento...');
-                TP.service('elementos').get({id:id},function (response) {
-                    $scope.elemento = response;
-                    $scope.elementos[id] = response;
+            $scope.getElemento = function (id) {
+                if($scope.elementos[id]) {
+                    $scope.elemento = $scope.elementos[id];
                     $scope.showElemento($scope.elemento);
-                },function (response) {
-                   LxNotificationService.error('El elemento no se cargo');
-                });
+                } else { 
+                    LxNotificationService.info('Cargando elemento...');
+                    TP.service('elementos').get({id:id},function (response) {
+                        $scope.elemento = response;
+                        $scope.elementos[id] = response;
+                        $scope.showElemento($scope.elemento);
+                    },function (response) {
+                       LxNotificationService.error('El elemento no se cargo');
+                    });
+                }
+            }
+
+            $scope.showElemento = function (elemento) {
+                LxDialogService.open($scope.vm.dialogId);
+            }
+
+            $scope.setPosition = function (x,y) {
+                $scope.vm.clasificacion = x;
+                $scope.vm.activeTab = y;
             }
         }
-
-        $scope.showElemento = function (elemento) {
-            LxDialogService.open($scope.vm.dialogId);
-        }
-
-        $scope.setPosition = function (x,y) {
-            $scope.vm.clasificacion = x;
-            $scope.vm.activeTab = y;
-        }
-    }])
+    ])
 
     .controller('ElementosCtrl', ['$scope','TP',function ($scope,TP) {
         $scope.data = {};
